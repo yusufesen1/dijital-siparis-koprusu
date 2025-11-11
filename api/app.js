f678adb99b500fb995e11ff4d079a057aceb5a47
Yusuf Mıraz Esen <yusufmiraz04@gmail.com>
2025-11-11 22:11:43 +0300

    Environments Commit

--- api/app.js
+++ api/app.js
@@ -1,3 +1,7 @@
+if (process.env.NODE_ENV != "production")
+  require('dotenv').config()
+
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
